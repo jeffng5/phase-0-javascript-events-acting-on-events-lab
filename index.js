97e3e07f2020f6895e7f0eb8254066cbc16fe0fb
jeffreykng@hotmail.com <jeffreyng@Jeffreys-MacBook-Pro.local>
2023-03-22 06:09:01 -0400

    tried some additional code

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+
+function moveDodgerRight() { 
+    const dodger = document.getElementById('dodger')
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const right= parseInt(leftNumbers,10);
+    dodger.style.left = `${right + 1}px`;
+document.addEventListener('keydown', function (event){
+if (leftNumbers < 679 && event.key === "ArrowRight" || leftNumbers >= 280){
+        moveDodgerRight()}
+
+
+})}
+
+
+
+function moveDodgerLeft(){
+    const dodger = document.getElementById('dodger')
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left= parseInt(leftNumbers, 10)
+    dodger.style.left = `${left - 1}px`
+document.addEventListener('keydown', function (e){ 
+    if (leftNumbers >= 280 && e.key === 'ArrowLeft' || leftNumbers < 679)
+    {moveDodgerLeft()}
+
+})
+}
+console.log(moveDodgerLeft())
+console.log(moveDodgerRight())
+//console.log(holdStill())
